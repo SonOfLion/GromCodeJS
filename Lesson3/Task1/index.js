fb845be5b4713e6aac65925f7af026906462b12f
SonOfLion <syplusplus@gmail.com>
2020-03-19 17:04:15 +0200

    1

--- Lesson3/Task1/index.js
+++ Lesson3/Task1/index.js
@@ -2,7 +2,7 @@
 +17;
 console.log('+17: ', +17); +
 +'77';
-console.log('+77: ', +'77');
+console.log('+77: ', "+'77'");
 
 // Бинарный плюс складывает строки или прибавляет числа
 10 + '5';
@@ -20,7 +20,7 @@ console.log("undefined + '5': ", undefined + '5');
 7 - null;
 console.log('7 - null: ', 7 - null);
 5 - undefined;
-console.log('5 - undefined:', 5 - undefined);
+console.log('5 - undefined: ', 5 - undefined);
 'text' + null;
 console.log("'text' + null: ", 'text' + null);
 let num = 17;
