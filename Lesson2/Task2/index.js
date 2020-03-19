b863e81992bf2adb8b8b37a24fb6a296b37eb8c0
SonOfLion <syplusplus@gmail.com>
2020-03-19 13:59:33 +0200

    2

---
+++ Lesson2/Task2/index.js
@@ -0,0 +1,31 @@
+const a = 17;
+const b = 17.17;
+const c = '17';
+const d = "17";
+const e = "I'm a string";
+const f = false;
+const g = true;
+const h = null;
+const i = undefined;
+const j = Symbol("I'm a Symbol");
+const k = { name: 'John' };
+const l = [1, 2, 3, 4];
+const m = function run() {
+    return 17;
+};
+console.log('a: ', typeof 17);
+console.log('b: ', typeof 17.17);
+console.log('c: ', typeof '17');
+console.log('d: ', typeof "17");
+console.log('e: ', typeof "I'm a string");
+console.log('f: ', typeof false);
+console.log('g: ', typeof true);
+console.log('h: ', typeof null);
+console.log('i: ', typeof undefined);
+console.log('j: ', typeof Symbol);
+console.log('k: ', typeof { name: 'John' });
+console.log('l: ', typeof [1, 2, 3, 4]);
+console.log('m: ', typeof
+    function run() {
+        return 17;
+    });
