b73c2dac003965745539e525f97285e03bf35b2d
SonOfLion <syplusplus@gmail.com>
2020-03-19 11:15:05 +0200

    2

--- Lesson.2/Task2/index.js
+++ Lesson.2/Task2/index.js
@@ -3,10 +3,10 @@
 const userName = 'John';
 const greeting = "I'm John";
 const _profession = 'Developer';
-const $ho%bby = 'Walking';
+const $hobby = 'Walking';
 const favouriteDessert = 'cake';
 let pet_ = 'dog';
-let super&Number;
-super&Number = 17;
+let superNumber;
+superNumber = 17;
 
 */
