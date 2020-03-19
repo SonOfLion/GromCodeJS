eed9c5e1bca06dc7ea4f1d95fab1c2a1b432c413
SonOfLion <syplusplus@gmail.com>
2020-03-19 16:55:31 +0200

    1

--- Lesson3/Task1/index.js
+++ Lesson3/Task1/index.js
@@ -20,16 +20,16 @@ console.log("undefined + '5': ", undefined + '5');
 7 - null;
 console.log('7 - null: ', 7 - null);
 5 - undefined;
-console.log('5 - undefined:', 5 - undefined);
+console.log('5 - undefined:', '5 - undefined');
 'text' + null;
 console.log("'text' + null: ", 'text' + null);
 let num = 17;
 
 // Постфиксная и префиксная форма записи
 ++num;
-console.log('++num: ', ++num);
+console.log('++num: ', '++num');
 num++;
-console.log('num++: ', num++);
+console.log('num++: ', 'num++');
 
 
 // математические операторы приводят операнды к числам
