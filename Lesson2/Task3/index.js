311687ce63c30f82984bbf7b2ddb492e7ee53c6d
SonOfLion <syplusplus@gmail.com>
2020-03-19 15:12:42 +0200

    3

--- Lesson2/Task3/index.js
+++ Lesson2/Task3/index.js
@@ -3,7 +3,10 @@ let anotherNumber = goodNumber;
 anotherNumber = 77;
 console.log(goodNumber);
 console.log(anotherNumber);
-let customer = { name: 'Jhon', age: 17 }
+const customer = {
+    name: 'John',
+    age: 17,
+};
 let anotherCustomer = customer;
 anotherCustomer.age = 20;
 console.log(customer.age);
