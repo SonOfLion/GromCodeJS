4560f4673bc3b279570063555967bcfaf51cb356
SonOfLion <syplusplus@gmail.com>
2020-03-19 12:29:17 +0200

    2

---
+++ Lesson.3/Taks.3/index.js
@@ -0,0 +1,33 @@
+const a = 17;
+console.log('a: ', typeof 17);
+const b = 17.17;
+console.log('b: ', typeof 17.17);
+const c = '17';
+console.log('c: ', typeof '17');
+const d = "17";
+console.log('d: ', typeof "17");
+const e = "I'm a string";
+console.log('e: ', typeof "I'm a string");
+const f = false;
+console.log('f: ', typeof false);
+const g = true;
+console.log('g: ', typeof true);
+const h = null;
+console.log('h: ', typeof null);
+const i = undefined;
+console.log('i: ', typeof undefined);
+const j = Symbol("I'm a Symbol");
+console.log('j: ', typeof Symbol);
+const k = { name: 'John' };
+console.log('k: ', typeof 'John');
+const l = [1, 2, 3, 4];
+console.log('l: ', typeof [1, 2, 3, 4]);
+const m = function run() {
+    return 17;
+
+};
+console.log('m: ', typeof
+    function run() {
+        return 17;
+
+    });
