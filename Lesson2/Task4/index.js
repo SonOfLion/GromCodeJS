3c8d6cc388824651c681ee3ce16b7499380490e3
SonOfLion <syplusplus@gmail.com>
2020-03-19 15:31:11 +0200

    4

---
+++ Lesson2/Task4/index.js
@@ -0,0 +1,8 @@
+let a = 4;
+let b = 2;
+console.log('a: ', a);
+console.log('b: ', b);
+console.log('a - b =', a - b);
+console.log('a + b =', a + b);
+console.log('a / b =', a / b);
+console.log('a * b =', a * b);
