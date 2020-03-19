ebdc236ace2104a059ced89bb4ff4b8b27e2af34
SonOfLion <syplusplus@gmail.com>
2020-03-19 13:52:48 +0200

    2

--- Lesson.2/Task.2/index.js
+++ Lesson.2/Task.2/index.js
@@ -1,33 +1,31 @@
 const a = 17;
-console.log('a: ', typeof 17);
 const b = 17.17;
-console.log('b: ', typeof 17.17);
 const c = '17';
-console.log('c: ', typeof '17');
 const d = "17";
-console.log('d: ', typeof "17");
 const e = "I'm a string";
-console.log('e: ', typeof "I'm a string");
 const f = false;
-console.log('f: ', typeof false);
 const g = true;
-console.log('g: ', typeof true);
 const h = null;
-console.log('h: ', typeof null);
 const i = undefined;
-console.log('i: ', typeof undefined);
 const j = Symbol("I'm a Symbol");
-console.log('j: ', typeof Symbol);
 const k = { name: 'John' };
-console.log('k: ', typeof 'John');
 const l = [1, 2, 3, 4];
-console.log('l: ', typeof [1, 2, 3, 4]);
 const m = function run() {
     return 17;
-
 };
+console.log('a: ', typeof 17);
+console.log('b: ', typeof 17.17);
+console.log('c: ', typeof '17');
+console.log('d: ', typeof "17");
+console.log('e: ', typeof "I'm a string");
+console.log('f: ', typeof false);
+console.log('g: ', typeof true);
+console.log('h: ', typeof null);
+console.log('i: ', typeof undefined);
+console.log('j: ', typeof Symbol);
+console.log('k: ', typeof { name: 'John' });
+console.log('l: ', typeof [1, 2, 3, 4]);
 console.log('m: ', typeof
     function run() {
         return 17;
-
     });
