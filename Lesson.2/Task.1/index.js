ec8194bf4697339769a0fc67391d1ddbeb5ee328
SonOfLion <syplusplus@gmail.com>
2020-03-19 13:12:52 +0200

    1,2

---
+++ Lesson.2/Task.1/index.js
@@ -0,0 +1,11 @@
+
+
+const userName = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
+
