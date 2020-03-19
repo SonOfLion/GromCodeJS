e7eacb0d386f1453695efb85752c7f482484035c
SonOfLion <syplusplus@gmail.com>
2020-03-19 14:54:04 +0200

    3

---
+++ Lesson2/Task3/index.js
@@ -0,0 +1,10 @@
+let goodNumber = 17;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
+console.log(goodNumber);
+console.log(anotherNumber);
+let customer = { name: 'Jhon', age: 17 }
+let anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
