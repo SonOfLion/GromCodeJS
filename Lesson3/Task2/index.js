1ac1015923d9839be92e526966748374869fe557
SonOfLion <syplusplus@gmail.com>
2020-03-20 09:56:08 +0200

    1

--- Lesson3/Task2/index.js
+++ Lesson3/Task2/index.js
@@ -1,6 +1,6 @@
 //code should be finished ;)
 
-console.log("17 to string is String(17)");
+console.log("17 to string is + String(17)");
 console.log("-17.17 to string is String(-17.17");
 console.log("false to string is String(false) ");
 console.log("null to string is String(null)");
@@ -14,9 +14,9 @@ console.log("true to number is Number(true)");
 console.log("false to number is Number(false)");
 console.log("null to number is Number(null)");
 console.log("undefined to number is Number(undefined)");
-console.log("'   20   ' to number is Number('20')");
+console.log("'   20   ' to number is Number('   20   ')");
 console.log("'      ' to number is Number ()");
-console.log("'   30d   ' to number is Number('30d')");
+console.log("'   30d   ' to number is Number('   30d   ')");
 
 /* == */
 console.log("null to boolean is Boolean(null)");
