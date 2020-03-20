f5912152330cb87974486aa797d6a9b921cf2d17
SonOfLion <syplusplus@gmail.com>
2020-03-20 10:04:51 +0200

    2

--- Lesson3/Task2/index.js
+++ Lesson3/Task2/index.js
@@ -1,6 +1,6 @@
 //code should be finished ;)
 
-console.log("'17 to string is' + String(17)");
+console.log("17 to string is" + String(17));
 console.log("'-17.17 to string is' + String(-17.17");
 console.log("'false to string is' + String(false) ");
 console.log("'null to string is' + String(null)");
