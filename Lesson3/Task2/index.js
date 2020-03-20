5ec19214bff64f7a60783653ef91608540c433b1
SonOfLion <syplusplus@gmail.com>
2020-03-20 10:08:02 +0200

    2

--- Lesson3/Task2/index.js
+++ Lesson3/Task2/index.js
@@ -1,11 +1,11 @@
 //code should be finished ;)
 
 console.log("17 to string is" + String(17));
-console.log("'-17.17 to string is' + String(-17.17");
-console.log("'false to string is' + String(false) ");
-console.log("'null to string is' + String(null)");
-console.log("'undefined to string is' + String(undefined)");
-console.log("'0 to string is' + String(0)");
+console.log("-17.17 to string is" + String(-17.17));
+console.log("false to string is" + String(false));
+console.log("null to string is" + String(null));
+console.log("undefined to string is" + String(undefined));
+console.log("0 to string is" + String(0));
 
 /* === */
 
