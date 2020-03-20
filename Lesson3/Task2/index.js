b42c34207ca767c26f7ce5bf45c34e9a74bd5edc
SonOfLion <syplusplus@gmail.com>
2020-03-20 10:13:20 +0200

    2

--- Lesson3/Task2/index.js
+++ Lesson3/Task2/index.js
@@ -9,22 +9,22 @@ console.log("0 to string is" + String(0));
 
 /* === */
 
-console.log("'17' to number is + Number('17')");
-console.log("true to number is + Number(true)");
-console.log("false to number is + Number(false)");
-console.log("null to number is + Number(null)");
-console.log("undefined to number is + Number(undefined)");
-console.log("'   20   ' to number is + Number('   20   ')");
-console.log("'      ' to number is + Number ()");
-console.log("'   30d   ' to number is + Number('   30d   ')");
+console.log("'17' to number is" + Number('17'));
+console.log("true to number is" + Number(true));
+console.log("false to number is" + Number(false));
+console.log("null to number is" + Number(null));
+console.log("undefined to number is" + Number(undefined));
+console.log("'   20   ' to number is" + Number('   20   '));
+console.log("'      ' to number is" + Number('      '));
+console.log("'   30d   ' to number is" + Number('   30d   '));
 
 /* == */
-console.log("null to boolean is + Boolean(null)");
-console.log("undefined to boolean is + Boolean(undefined)");
-console.log("0 to boolean is + Boolean(0)");
-console.log("-0 to boolean is + Boolean(-0)");
-console.log("NaN to boolean is + Boolean(Nan)");
-console.log("'' to boolean is + Boolean()");
-console.log("' ' to boolean is + Boolean()");
-console.log("17 to boolean is + Boolean(17)");
-console.log("'Hello' to boolean is + Boolean('Hello')");
+console.log("null to boolean is" + Boolean(null));
+console.log("undefined to boolean is" + Boolean(undefined));
+console.log("0 to boolean is" + Boolean(0));
+console.log("-0 to boolean is" + Boolean(-0));
+console.log("NaN to boolean is" + Boolean(NaN));
+console.log("'' to boolean is" + Boolean());
+console.log("' ' to boolean is" + Boolean());
+console.log("17 to boolean is" + Boolean(17));
+console.log("'Hello' to boolean is" + Boolean('Hello'));
