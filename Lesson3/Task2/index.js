ff6b1244a1b37d488d3f97c7917c3aa9a10c9ce3
SonOfLion <syplusplus@gmail.com>
2020-03-20 10:20:23 +0200

    2

--- Lesson3/Task2/index.js
+++ Lesson3/Task2/index.js
@@ -1,5 +1,3 @@
-//code should be finished ;)
-
 console.log("17 to string is" + String(17));
 console.log("-17.17 to string is" + String(-17.17));
 console.log("false to string is" + String(false));
@@ -7,7 +5,6 @@ console.log("null to string is" + String(null));
 console.log("undefined to string is" + String(undefined));
 console.log("0 to string is" + String(0));
 
-/* === */
 
 console.log("'17' to number is" + Number('17'));
 console.log("true to number is" + Number(true));
@@ -18,13 +15,12 @@ console.log("'   20   ' to number is" + Number('   20   '));
 console.log("'      ' to number is" + Number('      '));
 console.log("'   30d   ' to number is" + Number('   30d   '));
 
-/* == */
 console.log("null to boolean is" + Boolean(null));
 console.log("undefined to boolean is" + Boolean(undefined));
 console.log("0 to boolean is" + Boolean(0));
 console.log("-0 to boolean is" + Boolean(-0));
 console.log("NaN to boolean is" + Boolean(NaN));
 console.log("'' to boolean is" + Boolean());
-console.log("' ' to boolean is" + Boolean());
+console.log("' ' to boolean is" + Boolean(''));
 console.log("17 to boolean is" + Boolean(17));
 console.log("'Hello' to boolean is" + Boolean('Hello'));
