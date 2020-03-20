5351650cff27ac49f8cf840caefa82077e528dcf
SonOfLion <syplusplus@gmail.com>
2020-03-20 10:56:10 +0200

    3

--- Lesson3/Task3/index.js
+++ Lesson3/Task3/index.js
@@ -5,65 +5,65 @@ const b = 5;
 a > b;
 console.log("a > b: ", a > b);
 a <= b;
-console.log(a <= b);
+console.log("a <= b: ", a <= b);
 a == b;
-console.log(a == b);
+console.log("a == b: ", a == b);
 a != b;
-console.log(a != b);
+console.log("a != b: ", a != b);
 
 
 /* сравнение строк */
 
 'a' < 'b';
-console.log('a' < 'b');
+console.log("'a' < 'b': ", 'a' < 'b');
 'ab' > 'a';
-console.log('ab' > 'a');
+console.log("'ab' > 'a': ", 'ab' > 'a');
 
 /* строгое сравнение */
 
 17 === 1;
-console.log(17 === 1);
+console.log("17 === 1: ", 17 === 1);
 17 === 17;
-console.log(17 === 17);
+console.log("17 === 17: ", 17 === 17);
 17 === '17';
-console.log(17 === '17');
+console.log("17 === '17': ", 17 === '17');
 17 === true;
-console.log(17 === true);
+console.log("17 === true: ", 17 === true);
 '0' === '';
-console.log('0' === '');
+console.log("'0' === '': ", '0' === '');
 true === false;
-console.log(true === false);
+console.log("true === false: ", true === false);
 true === true;
-console.log(true === true);
+console.log("true === true: ", true === true);
 null === undefined;
-console.log(null === undefined);
+console.log("null === undefined: ", null === undefined);
 false === 0
-console.log(false === 0);
+console.log("false === 0: ", false === 0);
 
 
 /* нестрогое сравнение */
 
 17 == '17';
-console.log(17 == '17');
+console.log("17 == '17': ", 17 == '17');
 '0' == '';
-console.log('0' == '');
+console.log("'0' == '': ", '0' == '');
 0 == '';
-console.log(0 == '');
+console.log("0 == '': ", 0 == '');
 null == undefined;
-console.log(null == undefined);
+console.log("null == undefined: ", null == undefined);
 false == 0;
-console.log(false == 0);
+console.log("false == 0: ", false == 0);
 
 
 /* осторожно c null и undefined */
 
 undefined == null;
-console.log(undefined == null);
+console.log("undefined == null: ", undefiend == null);
 undefined == 0;
-console.log(undefined == 0);
+console.log("undefined == 0: ", undefined == 0);
 null == 0;
-console.log(null == 0);
+console.log("null == 0: ", null == 0);
 undefined < 0;
-console.log(undefined < 0);
+console.log("undefined < 0: ", undefined < 0);
 undefined > 0;
-console.log(undefined > 0);
+console.log("undefined > 0: ", undefined > 0);
