4323ff235129a34cdd51f200c90e48cec8af053b
SonOfLion <syplusplus@gmail.com>
2020-03-20 13:09:12 +0200

    5

---
+++ Lesson3/Task5/index.js
@@ -0,0 +1,32 @@
+if (true) console.log('I\'m in');
+
+if (false) {
+    console.log('I\'m not in');
+}
+
+if ('text') {
+    console.log('Yes');
+}
+
+if (null) {
+    console.log('Yes');
+} else {
+    console.log('Else');
+}
+
+let age = 19;
+
+if (age < 16) {
+    console.log('Too young');
+} else if (age >= 16 && age < 18) {
+    console.log('Almost ready');
+} else if (age >= 18 && age < 65) {
+    console.log('Ready');
+} else {
+    console.log('Else');
+}
+
+// Тернаный оператор ? : ( const result = условие ? значение1 : значение2; )
+let amount = 16;
+const result = amount > 17 ? 'full' : 'empty';
+console.log(result);
