5ea85f51ed2c07203f914bab63551e0728105356
SonOfLion <syplusplus@gmail.com>
2020-04-26 21:47:43 +0300

    2

--- Lesson4/Task2/index.js
+++ Lesson4/Task2/index.js
@@ -2,11 +2,11 @@ let m = 10;
 let n = 20;
 let result = 1;
 
-do {
+while (m <= n) {
     if (m % 2 === 1) {
         result *= m;
     }
     m++;
 }
-while (m < n);
 console.log('Result: ' + result);
+console.log(m, n);
