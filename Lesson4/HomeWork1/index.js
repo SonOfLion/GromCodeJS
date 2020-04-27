dcd92afc3edb4920d36dd1cca126978e53d409ad
SonOfLion <syplusplus@gmail.com>
2020-04-27 12:27:07 +0300

    1

--- Lesson4/HomeWork1/index.js
+++ Lesson4/HomeWork1/index.js
@@ -1,8 +1,8 @@
-let n = 50;
+let n = 10;
 let sum = 0;
 
 for (let i = 1; i <= n; i++) {
-    sum = sum + i + ' ';
+    sum = sum + i + '';
 }
 
-console.log('n = 50 это строка ' + sum);
+console.log('n = 10 это строка ' + sum);
