b1cda46dcb445a634858b571affc09f47f0b74cc
SonOfLion <syplusplus@gmail.com>
2020-04-27 14:57:41 +0300

    1

--- Lesson4/HW2/index.js
+++ Lesson4/HW2/index.js
@@ -2,7 +2,7 @@ let m = 10;
 
 let result = 0;
 
-for (let n = 50; n > m; n++) {
+for (let n = 40; n > 10; n++) {
     if (n, m % 2 === 0 && n, m % 4 !== 0) {
         result = n + m;
     } else if (n, m % 3 === 0) {
