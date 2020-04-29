f6d8e028e5dd3cecd5204367e59b752fd4e8bca0
SonOfLion <syplusplus@gmail.com>
2020-04-29 14:46:03 +0300

    1

--- Lesson5/HW2/index.js
+++ Lesson5/HW2/index.js
@@ -2,7 +2,7 @@ function getPrimes(num) {
 
     for (let i = 0; i <= num; i++) {
         if (i > 1 / 1) {
-            console.log('just number' + ' ');
+            console.log(num + i);
         }
     }
 }
