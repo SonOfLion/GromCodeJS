d9c1660b2eaa503f2a1afd9603dfcfca1f779801
SonOfLion <syplusplus@gmail.com>
2020-04-29 12:13:24 +0300

    1

--- Lesson5/HW1/index.js
+++ Lesson5/HW1/index.js
@@ -1,9 +1,7 @@
 function getSum(num1, num2) {
     let sum = 0;
-    for (let num1 = 0; num1 <= num2; i++) {
-        sum += i;
-        if (sum % 2 === 0) {
-            return sum;
-        }
+    sum = num1 + num2;
+    if (sum % 2 === 0) {
+        return sum;
     }
 }
