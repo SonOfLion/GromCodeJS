c3297a2a59f854e999fda051e76bf5eaa876e525
SonOfLion <syplusplus@gmail.com>
2020-04-30 12:50:32 +0300

    4

--- Lesson5/HW4/index.js
+++ Lesson5/HW4/index.js
@@ -1,7 +1,14 @@
 function compareSums(a, b, c, d) {
 
+    if (sum1 > sum2) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 function sum(from, to) {
-
+    let sum1 = a + b;
+    let sum2 = c + d;
 }
