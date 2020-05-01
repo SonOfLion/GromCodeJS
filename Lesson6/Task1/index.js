6fa212f224835fe34f9b8928f10b6a19aba0fb09
SonOfLion <syplusplus@gmail.com>
2020-05-01 23:47:15 +0300

    1

--- Lesson6/Task1/index.js
+++ Lesson6/Task1/index.js
@@ -1,8 +1,10 @@
-function getArrayBounds(arr) {
+const numbers = [11, 22, 44, 55, 66];
 
+function getArrayBounds(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
-
     return [arr.lenght, arr[0], arr[arr.lenght - 1]];
 }
+
+getArrayBounds(numbers);
