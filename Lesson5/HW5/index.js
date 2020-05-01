954e18040b08174cd7523e35fc0882c231feadce
SonOfLion <syplusplus@gmail.com>
2020-05-01 16:43:50 +0300

    5

--- Lesson5/HW5/index.js
+++ Lesson5/HW5/index.js
@@ -4,7 +4,7 @@ function findDivCount(a, b, n) {
     for (let i = a; i <= b; i++) {
         sum += i;
     }
-    if (sum % 2 === 0 && sum === n) {
+    if (i % 2 === 0 && i === n) {
         return sum;
     } else {
         return 0;
