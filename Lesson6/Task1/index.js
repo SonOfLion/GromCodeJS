dd831bc2b43dde1408fb46cea79fa195bf80641b
SonOfLion <syplusplus@gmail.com>
2020-05-01 23:40:02 +0300

    1

--- Lesson6/Task1/index.js
+++ Lesson6/Task1/index.js
@@ -1,4 +1,8 @@
-function getArrayBound(arr) {
+function getArrayBounds(arr) {
+
+    if (!Array.isArray(arr)) {
+        return null;
+    }
 
     return [arr.lenght, arr[0], arr[arr.lenght - 1]];
 }
