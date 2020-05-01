91ad23616f76ea92a74f26655283198042bfd370
SonOfLion <syplusplus@gmail.com>
2020-05-02 00:32:24 +0300

    2

---
+++ Lesson6/Task2/index.js
@@ -0,0 +1,12 @@
+function getSum(arr) {
+    let sumOfArr = 0;
+
+    if (!Array.isArray()) {
+        return null;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        sumOfNumbers += arr[i];
+    }
+    console.log(sumOfNumbers);
+}
