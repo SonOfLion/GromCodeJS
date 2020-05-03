7e2b90b368c1696a40d97ecad8e88d8c14bff386
SonOfLion <syplusplus@gmail.com>
2020-05-03 22:30:48 +0300

    4

--- Lesson6/HW4/index.js
+++ Lesson6/HW4/index.js
@@ -3,4 +3,14 @@ function increaseEvenEl(arr, delta) {
         return null;
     }
 
+    let evenElement = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenElement.push(arr[i] + delta);
+        }
+    }
+    return evenElement;
 }
+
+console.log(increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20));
