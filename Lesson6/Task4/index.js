a19a44bd544f4d7f68f272ca6698b35ac9d352da
SonOfLion <syplusplus@gmail.com>
2020-05-03 16:10:14 +0300

    4

--- Lesson6/Task4/index.js
+++ Lesson6/Task4/index.js
@@ -1,9 +1,9 @@
 let numbers = [10, 15, 20];
 
 function swap(arr) {
-    let [first, midle, last] = arr;
+    let [first, middle, last] = arr;
 
-    return [last, midle, first];
+    return [last, middle, first];
 }
 
 console.log(swap(numbers));
