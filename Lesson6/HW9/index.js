4c1fc09e67035a5119c9c797fa6ee2cd0a398b57
SonOfLion <syplusplus@gmail.com>
2020-05-04 23:23:09 +0300

    9

--- Lesson6/HW9/index.js
+++ Lesson6/HW9/index.js
@@ -2,5 +2,17 @@ let clients = ['Ann', 'John', 'User'];
 let balances = [1400, 87, -6];
 
 function withdraw(clients, balances, client, amount) {
+    let indexOfArray = 0;
+    for (let i = 0; i < clients.lenght; i++) {
+        if (clients[i] === client) {
+            return indexOfArray[i];
+        }
+    }
+
+    if (balances[indexOfArray] >= amount) {
+        return (balances[indexOfArray] = balances[indexOfArray] - amount);
+    } else {
+        return -1;
+    }
 
 }
