4965c5684d778b8ea3f4832fcc47c5081612c253
SonOfLion <syplusplus@gmail.com>
2020-05-04 21:43:40 +0300

    8

--- Lesson6/HW8/index.js
+++ Lesson6/HW8/index.js
@@ -27,9 +27,21 @@ function sortDesc(array) {
         return null;
     }
 
-
-
-
+    for (let i = array.length - 1; i > 0; i++) {
+        let count = 0;
+        for (let j = 0; j < i; j++) {
+            if (array[j] < array[j + 1]) {
+                let swap = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = swap;
+                count++;
+            }
+        }
+        if (count === 0) {
+            break;
+        }
+    }
+    return array;
 }
 
 console.log(sortDesc([3, 2, 6, 9]));
