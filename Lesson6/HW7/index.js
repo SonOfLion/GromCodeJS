5f757bb372928125d7593db4c410cd778511af21
SonOfLion <syplusplus@gmail.com>
2020-05-04 13:14:49 +0300

    7

--- Lesson6/HW7/index.js
+++ Lesson6/HW7/index.js
@@ -2,4 +2,28 @@ function checker(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
+
+    let minElOfArray = arr[0];
+    let maxElOfArray = arr[0];
+    let sumOfMinMaxArr = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (maxElOfArray < arr[i]) {
+            maxElOfArray = arr[i];
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (minElOfArray > arr[i]) {
+            minElOfArray = arr[i];
+        }
+    }
+
+    sumOfMinMaxArr = minElOfArray + maxElOfArray;
+    if (sum > 1000) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
