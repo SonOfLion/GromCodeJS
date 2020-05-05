86c8e6e418d34b85c30268a23475eeb26c7d4ef4
SonOfLion <syplusplus@gmail.com>
2020-05-05 15:07:29 +0300

    13

--- Lesson6/HW13/index.js
+++ Lesson6/HW13/index.js
@@ -2,4 +2,14 @@ function uniqueCount(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
+
+    for (let i = 0; i <= arr.length; i++) {
+        for (let j = i + 1; j <= arr.length; i++) {
+            if (arr[i] === arr[j]) {
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return arr.length;
 }
