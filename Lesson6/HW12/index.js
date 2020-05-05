bbc192fcff48eb1c89858b491872ae3b8e3eb004
SonOfLion <syplusplus@gmail.com>
2020-05-05 13:30:40 +0300

    12

--- Lesson6/HW12/index.js
+++ Lesson6/HW12/index.js
@@ -1,14 +1,16 @@
 function removeDuplicates(array) {
-    if (!Array.isArray(arr)) {
+    if (!Array.isArray(array)) {
         return null;
     }
 
     for (let i = 0; i < array.length; i++) {
         for (let j = i; j < array.length; j++) {
-            if (arr[i] === arr[j]) {
-                arr.splice;
+            if (array[i] === array[j]) {
+                array.splice(j);
             }
         }
     }
     return array;
 }
+
+console.log(removeDuplicates([1, 2, 3, 3, 4]));
