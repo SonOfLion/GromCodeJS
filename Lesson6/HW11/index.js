fa629ee1b9ab45c947646653c4de6fca8b7245a1
SonOfLion <syplusplus@gmail.com>
2020-05-05 12:53:42 +0300

    11

--- Lesson6/HW11/index.js
+++ Lesson6/HW11/index.js
@@ -1,3 +1,9 @@
-function includes() {
-
+let includes = function(arr, n) {
+    if (arr.includes(n)) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+console.log(includes([1, 2, 3], 6));
