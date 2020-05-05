1705a832ad8670e06ea93c8a00ab6e9fd74d9333
SonOfLion <syplusplus@gmail.com>
2020-05-05 12:36:08 +0300

    10

--- Lesson6/HW10/index.js
+++ Lesson6/HW10/index.js
@@ -1,3 +1,10 @@
-function getSubArray(a, b) {
+function getSubArray(arr, n) {
+    let newArr = [];
 
+    for (let i = 0; i < n; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
 }
+
+console.log(getSubArray([1, 2, 3, 4], 2));
