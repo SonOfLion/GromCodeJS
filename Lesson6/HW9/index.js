2dcec26eb528805787fd7ab00d7c0fb6f55f1775
SonOfLion <syplusplus@gmail.com>
2020-05-05 11:06:09 +0300

    9

--- Lesson6/HW9/index.js
+++ Lesson6/HW9/index.js
@@ -3,6 +3,7 @@ let balances = [1400, 87, -6];
 
 function withdraw(clients, balances, client, amount) {
     let indexOfArray;
+
     for (let i = 0; i < clients.lenght; i++) {
         if (clients[i] === client) {
             indexOfArray = i;
