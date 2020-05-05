b82715f511afa6dd687b64677e8683add294fd7a
SonOfLion <syplusplus@gmail.com>
2020-05-05 21:27:51 +0300

    1

--- Lesson7/Task1/index.js
+++ Lesson7/Task1/index.js
@@ -1,3 +1,10 @@
-function getSpecialNumbers(arr) {
+const getSpecialNumbers = numbers => {
+    let specialNumbers = [];
 
+    for (let i = 0; i < numbers; i++) {
+        if (numbers[i] % 3 === 0) {
+            specialNumbers.push();
+        }
+    }
+    return specialNumbers;
 }
