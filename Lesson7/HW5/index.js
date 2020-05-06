c8fb415f0a20191271a99bd5a967dc5e8ee443b8
SonOfLion <syplusplus@gmail.com>
2020-05-06 16:13:35 +0300

    5

--- Lesson7/HW5/index.js
+++ Lesson7/HW5/index.js
@@ -3,4 +3,11 @@ let balances = [1400, 87, -6];
 
 function withdraw(clients, balances, client, amount) {
 
+    if (balances[clients.indexOf(client)] >= amount)
+
+        return (balances[clients.indexOf(client)] = balances[clients.indexOf(client)] - amount);
+
+    return -1;
 }
+
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6]))
