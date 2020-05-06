e76653321134efaba278ff1cae22c1644d01430e
SonOfLion <syplusplus@gmail.com>
2020-05-06 17:28:05 +0300

    7

--- Lesson7/HW7/index.js
+++ Lesson7/HW7/index.js
@@ -1,10 +1,8 @@
-function filterNames(arr, text) {
-
-    arr.filter((array) => {
-        if (array.includes(text) && array.length > 5);
-    });
+let filterNames = (arr, text) => arr.filter((array) => {
+    if (array.includes(text) && array.length > 5);
     return true;
-}
+});
+
 
 let namesOfPeople = ['John', 'Olivia', 'Oleksandr', 'Emanuel', 'Vanya', 'Nystya', 'ya'];
 console.log(filterNames(namesOfPeople));
