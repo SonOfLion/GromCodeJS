faf298094ff20b7c5bf68046eaebfb4c22aa7001
SonOfLion <syplusplus@gmail.com>
2020-05-06 18:10:26 +0300

    8

--- Lesson7/HW8/index.js
+++ Lesson7/HW8/index.js
@@ -1,3 +1,8 @@
 function arrAverage(arr) {
     if (!Array.isArray(arr)) return null;
+
+    return arr.reduce((accumulator, element) => accumulator + element / element)
 }
+
+let numbers = [4, 6];
+console.log(arrAverage(numbers));
