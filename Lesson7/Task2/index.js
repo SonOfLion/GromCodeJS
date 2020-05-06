6de3916d8f679b47f158c9bac80a2a797e2ea949
SonOfLion <syplusplus@gmail.com>
2020-05-06 11:50:16 +0300

    2

--- Lesson7/Task2/index.js
+++ Lesson7/Task2/index.js
@@ -1,15 +1,5 @@
-const sortDesc = numbers => {
-    function compare(a, b) {
-        if (a < b) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }
-
-    numbers.sort(compare);
-
-    return numbers;
+const sortDesc = numbers =>
+    numbers.slice().sort((a, b) => b - a);
 }
 
 const arr = [1, 2, 4, 6, 9, 19, 29, 30, 23];
