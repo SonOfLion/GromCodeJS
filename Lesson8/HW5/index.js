6c5c3f1d7bc2ef2411b34ad2ef58f3dc9fddfee7
SonOfLion <syplusplus@gmail.com>
2020-05-07 15:04:33 +0300

    5

--- Lesson8/HW5/index.js
+++ Lesson8/HW5/index.js
@@ -10,3 +10,17 @@ function addPropertyV2(obj, key, value) {
     };
     return Object.assign(obj, idUser);
 }
+
+function addPropertyV3(obj, key, value) {
+    let idUser = {
+        key: [value]
+    };
+    return Object.assign({}, obj, idUser);
+}
+
+function addPropertyV4(obj, key, value) {
+    let idUser = {
+        key: [value]
+    };
+    return Object.assign({...obj }, idUser);
+}
