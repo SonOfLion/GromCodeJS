c15c6123c2d8033d576549def49fddd6c5225f7b
SonOfLion <syplusplus@gmail.com>
2020-05-07 13:47:41 +0300

    4

--- Lesson8/HW4/index.js
+++ Lesson8/HW4/index.js
@@ -1,17 +1,19 @@
 function addPropertyV1(userData, userId) {
-    userData = userId;
+    userData.id = userId;
 
     return userData;
 }
 
+
 function addPropertyV2(userData, userId) {
     let idUser = { id: userId };
 
-    return Object.assign({}, userData, idUser);
+    return Object.assign(userData, idUser);
 }
+
 
 function addPropertyV3(userData, userId) {
     let idUser = { id: userId };
 
-    return Object.assign(userData, idUser);
+    return Object.assign({}, userData, idUser);
 }
