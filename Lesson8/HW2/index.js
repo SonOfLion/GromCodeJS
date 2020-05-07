bdda27eb0cd57f42d505b389865179b6b1dafc25
SonOfLion <syplusplus@gmail.com>
2020-05-07 12:22:35 +0300

    2

--- Lesson8/HW2/index.js
+++ Lesson8/HW2/index.js
@@ -4,7 +4,7 @@ const user = {
 };
 
 /* присвойте объекту свойство hobby со значением 'football' с помощью точки */
-user.hobby = 'footbal';
+user.hobby = "footbal";
 
 const anotherKey = 'married';
 /* название свойства хранится в переменной anotherKey. Присвойте объекту такое свойство со значением false */
@@ -12,10 +12,10 @@ user[anotherKey] = false;
 /* присвойте объекту свойство 'favorite music' со значением 'rock' */
 user['favorite music'] = 'rock';
 /* присвойте объекту свойство 'address', которое будет пустым объкетом */
-user.adress = {};
+user.address = {};
 /* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
-user.adress.country = 'Ukraine';
+user.address.country = 'Ukraine';
 /* присвойте объекту в 'address' свойство 'building', со значением 17 */
-user.adress.building = 17;
+user.address.building = 17;
 /* выведите в консоль объект user */
 console.log(user);
