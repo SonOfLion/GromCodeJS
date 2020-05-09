ceac00947269c021a6a28a71f265eb16e517eb3b
SonOfLion <syplusplus@gmail.com>
2020-05-09 20:00:42 +0300

    7

--- Lesson8/HW7/index.js
+++ Lesson8/HW7/index.js
@@ -2,13 +2,7 @@ let obj1 = { name: 'Tom', age: 17, student: false };
 let obj2 = { name: 'Thomas', age: 17 };
 
 function compareObjects(obj1, obj2) {
-    let arr1 = Object.keys(obj1);
-    let arr2 = Object.keys(obj2);
-
-    if (arr1.length !== arr2.length) return false;
-
-    return arr1.reduce((acc, keys) =>
-        obj1[keys] === obj2[keys], true);
+    return obj1 !== obj2;
 }
 
 console.log(compareObjects(obj1, obj2));
