780566f07f1a91222dc7d65fb2d31f4bc78ed83e
SonOfLion <syplusplus@gmail.com>
2020-05-10 02:56:35 +0300

    7

--- Lesson8/HW7/index.js
+++ Lesson8/HW7/index.js
@@ -5,7 +5,7 @@ function compareObjects(obj1, obj2) {
     if (obj1.length !== obj2.length)
         return false;
 
-    return obj1.length === obj2;
+    return obj1.length === obj2.length;
 }
 
 console.log(compareObjects(obj1, obj2));
