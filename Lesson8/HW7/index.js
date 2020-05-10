a13abec8edc82fe11364db52d4509b8e2fa550c8
SonOfLion <syplusplus@gmail.com>
2020-05-11 00:54:31 +0300

    7

--- Lesson8/HW7/index.js
+++ Lesson8/HW7/index.js
@@ -1,10 +1,14 @@
-let obj1 = { name: 'Tom', age: 19 };
+let obj1 = { name: 'Tom', age: 17 };
 let obj2 = { name: 'Tom', age: 17 };
 
 function compareObjects(obj1, obj2) {
-    return obj1.length !== obj2.length;
+    var keysFirstObj = Object.keys(obj1);
+    var keysSecondObject = Object.keys(obj2);
 
-
+    if (keysFirstObj.length !== keysSecondObject.length) {
+        return false;
+    } else if (keysFirstObj.length === keysSecondObject.length) {
+        return true;
+    }
 }
-
 console.log(compareObjects(obj1, obj2));
