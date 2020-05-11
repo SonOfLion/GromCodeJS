158c138903baa75948f7dbfd434aa3598aca41ba
SonOfLion <syplusplus@gmail.com>
2020-05-11 14:58:55 +0300

    8

---
+++ Lesson8/HW8/index.js
@@ -0,0 +1,9 @@
+const keys = ['name', 'adress', 'age'];
+const values = ['Bob', 'Ukraine', 34];
+const result = buildObject(keys, values);
+
+function buildObject(keyList, valueList) {
+
+}
+
+console.table(keys, values, result);
