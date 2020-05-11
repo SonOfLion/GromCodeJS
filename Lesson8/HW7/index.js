cadd80a1eff3b4560f5f67bceee761cb85798192
SonOfLion <syplusplus@gmail.com>
2020-05-11 05:56:08 +0300

    7

--- Lesson8/HW7/index.js
+++ Lesson8/HW7/index.js
@@ -3,8 +3,8 @@ let obj2 = { name: 'Tom', age: 17 };
 //Сравнить два обьекта.
 //Метод length не подходит, ибо значение меняется а результат выдает true;
 function compareObjects(obj1, obj2) {
-    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-        return false;
+    if (Object.keys(obj1).length === Object.keys(obj2).length) {
+        return true;
     }
 
 }
