a826048f7f383c59c4d52301b2638489f3259757
SonOfLion <syplusplus@gmail.com>
2020-05-11 14:23:36 +0300

    7

--- Lesson8/HW7/index.js
+++ Lesson8/HW7/index.js
@@ -1,5 +1,7 @@
 let obj1 = { name: 'Tom', age: 17 };
-let obj2 = { name: 'Tom', age: 17 };
+let obj2 = { name: 'Bob', age: 17 };
+let obj3 = { name: 'Bob', age: 17, student: false };
+let obj4 = { name: 'Tom', age: 17 };
 //Сравнить два обьекта.
 //Метод length подходит только для ключей, ибо можно измерить длину ключа;
 //Нужно перебрать значения обьектов циклом.
@@ -8,9 +10,7 @@ function compareObjects(obj1, obj2) {
     for (let key in obj1) {
         if (obj1[key] !== obj2[key]) return false;
     }
-    if (obj1.length === obj2.length) return true;
-
-
+    if (obj1.length !== obj2.length) return true;
 }
 
 console.log(compareObjects(obj1, obj2));
