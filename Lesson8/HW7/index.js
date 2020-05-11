23d20b52f41b361ea7dfdffbaede2c9e3e2a2563
SonOfLion <syplusplus@gmail.com>
2020-05-11 09:16:41 +0300

    7

--- Lesson8/HW7/index.js
+++ Lesson8/HW7/index.js
@@ -7,10 +7,6 @@ function compareObjects(obj1, obj2) {
     for (let key in obj1) {
         if (obj1[key] !== obj2[key]) return false;
     }
-
-    for(let i of obj1){
-        if(obj1[key] !== obj2[key]) return false;
-    }
     return true;
 }
 
