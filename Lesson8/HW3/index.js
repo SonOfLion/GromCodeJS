fe0d361611b9710f43db46dec9c815ab43592366
SonOfLion <syplusplus@gmail.com>
2020-05-12 13:35:32 +0300

    all

--- Lesson8/HW3/index.js
+++
@@ -1,3 +0,0 @@
-const getKeys = obj => {
-    return Object.keys(obj).forEach(elem => console.log(elem));
-}
--- Lesson8/HW8/index.js
+++
@@ -1,9 +0,0 @@
-const keys = ['name', 'adress', 'age'];
-const values = ['Bob', 'Ukraine', 34];
-const result = buildObject(keys, values);
-
-function buildObject(keyList, valueList) {
-
-}
-
-console.table(keys, values, result);
