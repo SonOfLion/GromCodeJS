99f8e641bdbff6e870558410d34e0966ea91a7d4
SonOfLion <syplusplus@gmail.com>
2020-05-12 18:27:52 +0300

    9

--- Lesson9/Task00/index.js
+++ Lesson9/Task00/index.js
@@ -1,3 +1,20 @@
-function pickProps(obj, listWords) {
+// 1.Перебрать обьекты на ключи.
+// 2.Перебрать массви на равенство с ключом.
+// 3.Если 
 
+
+function pickProps(obj, arr) {
+    let emptyObj = {};
+
+    for (let keys in obj) {
+        for (let el in arr) {
+            if (keys === el) {
+                emptyObj = {};
+            }
+        }
+    }
+    return emptyObj;
 }
+let checkObj = { a: 1, b: 2, c: 3 };
+let checkArr = ['a', 'c'];
+console.log(pickProps(checkObj, checkArr));
