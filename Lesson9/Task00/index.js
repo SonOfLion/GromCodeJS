d66cf0d5149e1b3fb9c71da5496083963624c91a
SonOfLion <syplusplus@gmail.com>
2020-05-12 19:07:56 +0300

    9

--- Lesson9/Task00/index.js
+++ Lesson9/Task00/index.js
@@ -3,18 +3,20 @@
 // 3.Если функция не может добавить не одного значения, вернуть пустой объект.
 
 
+
 function pickProps(obj, arr) {
     let emptyObj = {};
 
     for (let keys in obj) {
         for (let el in arr) {
             if (keys === el) {
-                emptyObj = {};
+                emptyObj[keys] = obj[keys];
             }
         }
     }
     return emptyObj;
 }
+
 let checkObj = { a: 1, b: 2, c: 3 };
 let checkArr = ['a', 'c'];
 console.log(pickProps(checkObj, checkArr));
