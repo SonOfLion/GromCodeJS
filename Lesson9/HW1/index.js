477c9f0b6b9cc99d82620e7932ea127ad867b8e1
SonOfLion <syplusplus@gmail.com>
2020-05-13 15:38:30 +0300

    3

---
+++ Lesson9/HW1/index.js
@@ -0,0 +1,15 @@
+//Создать массив массивов.use Object.entries();
+//Вернуть массив пользоватлей с сортировкой по возрасту по возрастанаю.map(),sort();
+
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54,
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17,
+    }
+};
+
+function getCustomersList(users)
