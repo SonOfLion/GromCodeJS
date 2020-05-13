df2014ebeef6a3ee715bddedb7d17d8b7002331a
SonOfLion <syplusplus@gmail.com>
2020-05-13 12:15:44 +0300

    1

---
+++ Lesson9/Task1/index.js
@@ -0,0 +1,29 @@
+//getAdults
+
+// const users = {
+//     name: 'Tom',
+//     age: 17,
+// }
+
+//Object.keys().
+//Object.values().
+//Object.entries().
+
+//console.table(Object.entries(users));
+
+const users = {
+    'Tom': 17,
+    'John Doe': 19,
+    'Bob': 18,
+};
+
+const getAdults = usersObj => {
+    const usersArray = Object.entries(usersObj);
+    const filteredUsersArray = usersArray
+        .filter(users => users[1] > 18);
+
+    const usersName = filteredUsersArray
+        .map(users => users[0]);
+
+    return usersName;
+}
