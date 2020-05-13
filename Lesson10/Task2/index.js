c95a17e51933aa81ab3a4ff34f8fc59261d5084d
SonOfLion <syplusplus@gmail.com>
2020-05-13 21:44:40 +0300

    2

---
+++ Lesson10/Task2/index.js
@@ -0,0 +1,21 @@
+let numbers = [1, 2, 3, 41.23, 123, null, undefined, Infinity, 'text']
+
+function getParsedIntegers(arr) {
+    return arr.map((num) => Number.parseInt(num));
+}
+console.log(getParsedIntegersV2(numbers));
+
+function getParsedIntegersV2(arr) {
+    return arr.map((num) => parseInt(num));
+}
+console.log(getParsedIntegersV2(numbers));
+
+function getParsedFloats(arr) {
+    return arr.map((num) => Number.parseFloat(num));
+}
+console.log(getParsedFloats(numbers));
+
+function getParsedFloatsV2(arr) {
+    return arr.map((num) => parseFloat(num));
+}
+console.log(getParsedFloatsV2(numbers));
