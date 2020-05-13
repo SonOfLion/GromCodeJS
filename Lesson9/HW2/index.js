ef142f1d433fa2657a30f4b74079f771c4cc49d1
SonOfLion <syplusplus@gmail.com>
2020-05-13 18:43:34 +0300

    2

---
+++ Lesson9/HW2/index.js
@@ -0,0 +1,24 @@
+let rooms = {
+    room1: [
+        { name: 'room1 name1' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
+    ],
+    room2: [
+        { name: 'room2 name1' },
+        { name: 'room2 name2' },
+        { name: 'room2 name3' },
+    ],
+    room3: [
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+    ]
+};
+
+function getPeople(obj) {
+
+}
+
+console.table(rooms);
