4a80f71dc0ac8cfe3201858871e3e30e6b8d81f3
SonOfLion <syplusplus@gmail.com>
2020-05-13 21:26:44 +0300

    1

--- Lesson10/Task1/index.js
+++ Lesson10/Task1/index.js
@@ -1,25 +1,31 @@
+const numbers = [15, 9.2, null, undefined, '19', Infinity, 12319203.13123];
+
 function getFiniteNumber(arr) {
-    return Number.isFinite(arr);
+    return arr.filter((num) => Number.isFinite(num));
 }
 
-console.log(getFiniteNumber('9'));
+console.log(getFiniteNumber(numbers));
 
 function getFiniteNumberV2(arr) {
-    return arr.filter(num).isFinite(arr);
+    return arr.filter((num) => isFinite(num));
 }
 
-console.log(getFiniteNumberV2);
+console.log(getFiniteNumberV2(numbers));
 
 function getNan(arr) {
-    return Number.isNaN(arr);
+    return arr.filter((num) => Number.isNaN(num));
 }
 
-console.log(getFiniteNumberV2);
+console.log(getNan(numbers));
 
 function getNanV2(arr) {
-    return arr.filter(num).isNaN(arr);
+    return arr.filter((num) => isNaN(arr));
 }
 
+console.log(getNanV2(numbers));
+
 function getIntegers(arr) {
-    return Number.isInteger(arr);
+    return arr.filter((num) => Number.isInteger(num));
 }
+
+console.log(getIntegers(numbers));
