eea550e03b6de10e02018b4a8b5445730eb03170
SonOfLion <syplusplus@gmail.com>
2020-05-14 17:47:20 +0300

    2

--- Lesson10/HW2/index.js
+++ Lesson10/HW2/index.js
@@ -2,12 +2,21 @@
 //output: arr,length, float,float;
 //Eсли в промежутке нет целых чисел вернуть null.
 //Нужно вернуть массив целых чисел причем правильной длины.
-//
+//Создать пустой массив, создать цикл.
+//Элементы массива округлить вниз.
 
 
 function getRandomNumbers(len, start, end) {
-    const intNum = end - start < 1 && Math.ceil(end) === Math.ceil(start);
+    const intNum = end - start < 1 && Math.ceil(end) === Math.ceil(start); //проверка на четность
     if (intNum) {
         return null;
     }
+
+    let emptyArr = []; //Пустой массив
+
+    for (let i = 0; i < len; i++) { //Цикл
+        emptyArr[i] = Math.floor()
+    };
+
+    return emptyArr;
 }
