8066aec783e721afa38b686ebdc90699021bf819
SonOfLion <syplusplus@gmail.com>
2020-05-14 12:00:56 +0300

    3

--- Lesson10/Task3/index.js
+++ Lesson10/Task3/index.js
@@ -1,4 +1,4 @@
-const multiRound = (num) => [Math.floor(num * 100) / 100,
+const multiRound = num => [Math.floor(num * 100) / 100,
     Math.round(num * 100) / 100,
     Math.ceil(num * 100) / 100,
     Math.trunc(num * 100) / 100,
