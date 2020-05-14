8de4742526cba7ea65999d764be995232577de19
SonOfLion <syplusplus@gmail.com>
2020-05-14 17:24:50 +0300

    2

--- Lesson10/HW2/index.js
+++ Lesson10/HW2/index.js
@@ -1,8 +1,13 @@
 //input:length, float,float;
 //output: arr,length, float,float;
+//Eсли в промежутке нет целых чисел вернуть null.
+//Нужно вернуть массив целых чисел причем правильной длины.
+//
 
 
 function getRandomNumbers(len, start, end) {
-    if (start > end) return null;
-
+    const intNum = end - start < 1 && Math.ceil(end) === Math.ceil(start);
+    if (intNum) {
+        return null;
+    }
 }
