02d34c8523df1168622cbe920edc7c3c7b5d17ef
SonOfLion <syplusplus@gmail.com>
2020-05-14 12:47:07 +0300

    3

--- Lesson10/Task3/index.js
+++ Lesson10/Task3/index.js
@@ -1,7 +1,7 @@
 const multiRound = num => [
     Math.floor(num * 100) / 100,
-    Math.round(num),
-    Math.ceil(num),
-    Math.trunc(num),
+    Math.round(num * 10) / 100,
+    Math.ceil(num * 10) / 100,
+    Math.trunc(num * 10) / 100,
     Number(Math.PI.toFixed(2)),
 ];
