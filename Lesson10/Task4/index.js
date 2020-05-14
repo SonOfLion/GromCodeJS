2c2d34d8efb08be0eb92d555308f76bb7b7fffd1
SonOfLion <syplusplus@gmail.com>
2020-05-14 14:24:06 +0300

    3

--- Lesson10/Task4/index.js
+++ Lesson10/Task4/index.js
@@ -1,5 +1,5 @@
 const getMaxAbsoluteNumber = arr => {
-    if (!Array.isArray(arr)) return null;
+    if (!Array.isArray(arr) || arr.length === 0) return null;
 
     const absoluteValues = arr
         .map(num => Math.abs(num));
