8d673c286fa948c917c8b79d8e93dab4dea55b8b
SonOfLion <syplusplus@gmail.com>
2020-05-15 15:12:08 +0300

    2

--- Lesson11/Task2/index.js
+++ Lesson11/Task2/index.js
@@ -4,15 +4,15 @@
 function sortContacts(arr, logicTrue = true) {
     if (!Array.isArray(arr)) return null;
 
+    let result = arr.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+
     if (logicTrue === false) {
         sort((a, b) => {
             return b.name.localeCompare(a.name);
         });
     }
-
-    let result = arr.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-    });
 
     return result;
 }
