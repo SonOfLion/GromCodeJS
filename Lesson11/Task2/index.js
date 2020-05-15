cfae701dab701fcabbd7d511b087e94a7ddf3dab
SonOfLion <syplusplus@gmail.com>
2020-05-15 15:05:46 +0300

    2

---
+++ Lesson11/Task2/index.js
@@ -0,0 +1,38 @@
+//input:arr, logicFalse/True;
+//ouput:arr, if True a === b, else false b === a;
+
+function sortContacts(arr, logicTrue = true) {
+    if (!Array.isArray(arr)) return null;
+
+    if (logicTrue === false) {
+        sort((a, b) => {
+            return b.name.localeCompare(a.name);
+        });
+    }
+
+    let result = arr.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+
+    return result;
+}
+
+const contacts = [{
+        name: 'Tom',
+        phoneNumber: '333-333-4444',
+    },
+    {
+        name: 'Ann',
+        phoneNumber: '345-23-13-789',
+    },
+    {
+        name: 'Tom',
+        phoneNumber: '333-333-5555',
+    },
+    {
+        name: 'Tom',
+        phoneNumber: '989-232-434',
+    },
+];
+
+console.log(sortContacts(contacts));
