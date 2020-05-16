0154a694f670b99e688b0a4e842191566b4f5444
SonOfLion <syplusplus@gmail.com>
2020-05-16 18:11:39 +0300

    2

--- Lesson11/HW2/index.js
+++ Lesson11/HW2/index.js
@@ -5,9 +5,9 @@
 const countOccurrences = (str, substr) => {
     if (substr === '') return null;
 
-    return str.split(substr).length;
+    return str.split(substr).length - 1;
 }
 
 
-let name = 'JavaScript';
-console.log(countOccurrences(name));
+let name = '123.123.123.123';
+console.log(countOccurrences(name, 2));
