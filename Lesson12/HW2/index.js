116cf63ee8c108e7981da6b2059a2141b06a4050
SonOfLion <syplusplus@gmail.com>
2020-05-17 21:42:34 +0300

    3

--- Lesson12/HW2/index.js
+++ Lesson12/HW2/index.js
@@ -1,26 +1,9 @@
 //input:arr;
 //output:arr[$+numb];
 
-// function cleanTransactionsList(arr) {
-//     return arr.filter(elements => isFinite(elements))
-//         .map(elements => '$' + Number(elements).toFixed(2));
-// }
-
-
-
-const cleanTransactionsList = arr => {
-
-    let result = arr.map(Number);
-    console.log(result);
-    let arrr = [];
-    for (let value of result) {
-
-        if (!Number.isNaN(value)) {
-            arrr.push(`$${value.toFixed(2)}`);
-        }
-
-    }
-    console.log(arrr);
-    return arrr;
+function cleanTransactionsList(arr) {
+    return arr.filter(elements => isFinite(elements))
+        .map(elements => '$' + Number(elements).toFixed(2));
 }
+
 console.log(cleanTransactionsList([' 1.9 ', ' 16.4', 'dollar']));
