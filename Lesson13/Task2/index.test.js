f342592d264af2bb5031dc12fdbdb993d7ec3383
SonOfLion <syplusplus@gmail.com>
2020-05-18 12:35:38 +0300

    2

--- Lesson13/Task2/index.test.js
+++ Lesson13/Task2/index.test.js
@@ -1,4 +1,4 @@
-import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
+import getSum, { getSquaredArray, getOddNumbers } from '../calculator.js';
 
 it('should get squared numbers', () => {
     const result = getSquaredArray([1, 2, 3]);
