5f7be517e5d3d2ea0ffc0cfe94bf54b9ac45051f
SonOfLion <syplusplus@gmail.com>
2020-05-18 13:01:15 +0300

    2

--- Lesson13/Task2/calculator.js
+++ Lesson13/Task2/calculator.js
@@ -1,9 +1,6 @@
-export const getSquaredArray = arr => {
-    return arr.map(num => num * num);
-};
+export const getSquaredArray = arr => arr.map(num => num * num);
 
-export const getOddNumbers = arr => {
-    return arr.filter(num => (num % 2 === 1));
-};
+export const getOddNumbers = arr => arr.filter(num => (num % 2 === 1));
+
 
 export default (a, b) => a + b;
