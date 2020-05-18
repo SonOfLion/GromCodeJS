f29a3a26478d885b6a48f72ccca066df5744cb3d
SonOfLion <syplusplus@gmail.com>
2020-05-18 13:16:04 +0300

    2

--- Lesson13/Task2/index.test.js
+++ Lesson13/Task2/index.test.js
@@ -13,7 +13,7 @@ it('should keep odd numbers only', () => {
 });
 
 it('should get sum of numbers', () => {
-    const result = getSum([8, 4]);
+    const result = getSum(8, 4);
 
     expect(result).toEqual(12);
 });
