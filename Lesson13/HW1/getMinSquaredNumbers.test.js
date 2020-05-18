7eca6c5208be6be3e9f1b067cfe3a8ee651aeac0
SonOfLion <syplusplus@gmail.com>
2020-05-18 14:06:35 +0300

    1

--- Lesson13/HW1/getMinSquaredNumbers.test.js
+++ Lesson13/HW1/getMinSquaredNumbers.test.js
@@ -3,5 +3,5 @@ import { getMinSquaredNumbers } from './getMinSquaredNumbers.js';
 it('should get min numbers of array', () => {
     const result = getMinSquaredNumbers([13, 23, -23, 78]);
 
-    expected(result).toEqual(9);
+    expected(result).toEqual(4);
 });
