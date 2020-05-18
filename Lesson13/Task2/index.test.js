d9f52d887d23aff335f525e27750faa62e2d0911
SonOfLion <syplusplus@gmail.com>
2020-05-18 13:06:38 +0300

    2

--- Lesson13/Task2/index.test.js
+++ Lesson13/Task2/index.test.js
@@ -2,15 +2,18 @@ import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
     const result = getSquaredArray([1, 2, 3]);
+
     expect(result).toEqual([1, 4, 9]);
 });
 
 it('should keep odd numbers only', () => {
     const result = getOddNumbers([1, 2, 3, 4, 5]);
+
     expect(result).toEqual([1, 3, 5]);
 });
 
 it('should get sum of numbers', () => {
     const result = getSum([8, 4]);
+
     expect(result).toEqual(12);
 });
