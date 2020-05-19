2ac466c51d5c941a65649cd096fbc4c1cf5cf10b
SonOfLion <syplusplus@gmail.com>
2020-05-19 17:01:58 +0300

    2

---
+++ Lesson14/Task2/index.js
@@ -0,0 +1,38 @@
+//messendger
+
+let message = 'Hi!';
+
+const createMessenger = () => { //функция для создания сообщений
+
+    function sendMessage(name) { //создали функции отправки собщений
+        let message = 'Just learn it';
+        const sender = 'Gromcode';
+
+        console.log(`${name}, ${message}. Your ${sender}`);
+    }
+
+    function setMessage(text) { //функция с текстом 
+        message = text;
+    }
+
+    function setSender(text) {
+        message = text;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    };
+};
+
+const messenger1 = createMessenger();
+
+messenger1.sendMessage('Bob');
+
+const messenger2 = createMessenger();
+
+messenger2.sendMessage('Tom'); //к второй переменной применели метод (отправки сообщения).
+messenger2.setMessage('Hello'); //применили метод с использованием (сообщения).
+
+messenger1.sendMessage('Ann');
