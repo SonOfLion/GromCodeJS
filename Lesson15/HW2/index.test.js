56af76090b91edfdca605389e6f6eb8df36adb15
SonOfLion <syplusplus@gmail.com>
2020-05-21 11:43:52 +0300

    1

--- Lesson15/HW2/index.test.js
+++ Lesson15/HW2/index.test.js
@@ -1,7 +1,7 @@
 import { createLogger } from './index.js';
 
 it('should show message warn', () => {
-    let err = createLogger;
+    let err = createLogger();
     err.error('Error 303!');
 
     expect(err).toEqual('Error 303!');
---
+++ Lesson16/Task1/index.js
@@ -0,0 +1,11 @@
+let a = 17;
+
+if (a) {
+
+    let a = 1;
+
+    console.log(a);
+
+}
+
+console.log(a);
