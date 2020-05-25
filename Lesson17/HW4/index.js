26e9553b1eaa12876216fe36785ea44a0f7c77c3
SonOfLion <syplusplus@gmail.com>
2020-05-25 12:06:54 +0300

    4

---
+++ Lesson17/HW4/index.js
@@ -0,0 +1,17 @@
+export default user = {
+    firstName: 'John',
+    lastName: 'Doe',
+
+    getFullName() {
+        return `user ${this.firsName} ${this.lastName}`
+    },
+
+    setFullName(fullname) {
+        let cutOff = fullname.split(' ');
+        this.firstName = [0];
+        this.lastName = [1];
+        return cutOff;
+    },
+};
+
+console.log(user.setFullName('Leonid Pistryi'));
