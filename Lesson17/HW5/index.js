07f968c19e2d071665cf1b6f581b118858c70042
SonOfLion <syplusplus@gmail.com>
2020-05-25 16:13:23 +0300

    5

--- Lesson17/HW5/index.js
+++ Lesson17/HW5/index.js
@@ -8,12 +8,11 @@ export const timer = {
     startTimer() { //
         this.counterOfTimer.setInterval(() => {
             this.secondPassed + 1;
-            if (this.secondPassed === '60') { //Если таймер достигнет === 60 сек, его нужно обнулить до 0 и заного запустить цикл
-                this.secondPassed === '0';
+            if (this.secondPassed === 60) { //Если таймер достигнет === 60 сек, его нужно обнулить до 0 и заного запустить цикл
+                this.secondPassed === 0;
                 this.secondPassed++;
             }
         }, 1000);
-
     },
 
     resetTimer() { //Must stoped timer and refresh property of timer
