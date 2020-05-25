f513ac4fc4df4440bbcd13b356aed097e6d710a2
SonOfLion <syplusplus@gmail.com>
2020-05-25 16:11:17 +0300

    5

--- Lesson17/HW5/index.js
+++ Lesson17/HW5/index.js
@@ -1,12 +1,22 @@
+//Нужно создать таймер с памятью
+
 export const timer = {
     secondPassed = 0,
     mindPassed = 0,
+    counterOfTimer = 0,
 
-    startTimer() {
-        '${secondPassed}' + 1;
+    startTimer() { //
+        this.counterOfTimer.setInterval(() => {
+            this.secondPassed + 1;
+            if (this.secondPassed === '60') { //Если таймер достигнет === 60 сек, его нужно обнулить до 0 и заного запустить цикл
+                this.secondPassed === '0';
+                this.secondPassed++;
+            }
+        }, 1000);
+
     },
 
-    resetTimer() {
+    resetTimer() { //Must stoped timer and refresh property of timer
         this.secondPassed = 0;
         this.mindPassed = 0;
     },
