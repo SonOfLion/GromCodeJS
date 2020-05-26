694f708461b3a5cad23c5b4650f42e3dfdf667d7
SonOfLion <syplusplus@gmail.com>
2020-05-26 15:52:51 +0300

    3

---
+++ Lesson18/Task3/index.js
@@ -0,0 +1,15 @@
+//pseudo array
+
+function sumOfSquares(...numbers) {
+    // console.log(typeof arguments);
+
+    return [...arguments].reduce((acc, elem) => {
+        return acc + elem;
+    }, 0);
+}
+
+// sumOfSquares(1, 2, 3, 4);
+
+// [].reduce((acc, elem) => { return }, 0);
+
+console.log(sumOfSquares(14, 23, 4));
