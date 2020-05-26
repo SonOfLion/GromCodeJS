08844401bf5cd2e5f1831d37d0364e8dffe98ea7
SonOfLion <syplusplus@gmail.com>
2020-05-26 13:10:27 +0300

    1

--- Lesson18/Task1/index.js
+++ Lesson18/Task1/index.js
@@ -6,14 +6,14 @@ export const event = {
         { name: 'John', email: 'b@gmail.com', age: 18 },
     ],
 
-    message: 'Welcome to the party',
+    message: 'Welcome to the party!',
 
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
+                email,
                 text: `Dear ${name}! ${this.message}`,
-                email
             }));
     }
 }
