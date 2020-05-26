58a5b0de27f16f64d9e3834584f870c447b7409f
SonOfLion <syplusplus@gmail.com>
2020-05-26 15:12:23 +0300

    2

--- Lesson18/Task2/index.js
+++ Lesson18/Task2/index.js
@@ -4,11 +4,11 @@ export const wallet = {
     transaction: [1, 2, 3, 4, 5, 13, 78, 99],
 
     getMax() {
-        return Math.max.apply(null, this.transaction);
+        return Math.max(...this.transaction);
     },
 
     getMin() {
-        return Math.min.apply(null, this.transaction);
+        return Math.min(...this.transaction);
     },
 }
 
