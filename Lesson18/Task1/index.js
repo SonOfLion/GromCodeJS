862899a79dc702c14f9e1e06bb4575df14892e63
SonOfLion <syplusplus@gmail.com>
2020-05-26 13:12:24 +0300

    1

--- Lesson18/Task1/index.js
+++ Lesson18/Task1/index.js
@@ -2,20 +2,19 @@
 
 export const event = {
     guests: [
-        { name: 'Tom', email: 't@gmail.com', age: 17 },
-        { name: 'John', email: 'b@gmail.com', age: 18 },
+        { name: "Tom", email: "t@gmail.com", age: 17 },
+        { name: "John", email: "j@gmail.com", age: 18 },
     ],
-
-    message: 'Welcome to the party!',
-
+    message: "Welcome to the party!",
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
                 email,
-                text: `Dear ${name}! ${this.message}`,
+                message: `Dear ${name}! ${this.message}`,
+
             }));
-    }
+    },
 }
 
-console.log(event.getInvitations());
+// console.log(event.getInvitations());
