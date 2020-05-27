956180eef19423fb6cb055e8f274a4898c9fb539
SonOfLion <syplusplus@gmail.com>
2020-05-27 21:46:56 +0300

    4

--- Lesson19/Task4/index.js
+++ Lesson19/Task4/index.js
@@ -1,26 +1,24 @@
 'use strict';
 
-
+export default user;
 
 const user = {
     firstName: 'John',
     lastName: 'Doe',
 
-    get fullNameOfUser() {
+    get fullName() {
         return `${this.firstName} ${this.lastName}` //return fullname of user;++
     },
 
-    set fullNameOfUser(property) { //split username by property ++
+    set fullName(property) { //split username by property ++
         const [firstName, lastName] = property.split(' ');
         this.firstName = firstName;
         this.lastName = lastName;
     }, //return undefined;++
 };
 
-export default user;
+console.log(user.fullName);
 
-console.log(user.fullNameOfUser);
+user.fullName = 'Leonid Pistryi';
 
-user.fullNameOfUser = 'Leonid Pistryi';
-
-console.log(user.fullNameOfUser);
+console.log(user.fullName);
