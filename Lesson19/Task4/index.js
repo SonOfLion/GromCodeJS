d9114196212fddc65642398e847642a1aa2d2cec
SonOfLion <syplusplus@gmail.com>
2020-05-27 21:27:29 +0300

    4

---
+++ Lesson19/Task4/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+export const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+
+    get fullNameOfUser() {
+        return `${this.firstName} ${this.lastName}` //return fullname of user;
+    },
+
+    set fullNameOfUser(property) { //split username by property
+        const [firstName, lastName] = property.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }, //return undefined;
+};
+
+console.log(user.fullNameOfUser);
+
+user.fullNameOfUser = 'Leonid Pistryi';
+
+console.log(user.fullNameOfUser);
