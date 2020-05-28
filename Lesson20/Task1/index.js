ef2dd65a8bfaee2de62e2b59b462e93a7cba68a0
SonOfLion <syplusplus@gmail.com>
2020-05-28 16:21:18 +0300

    1

---
+++ Lesson20/Task1/index.js
@@ -0,0 +1,29 @@
+export class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+    setAge(value) {
+        if (value < 0) return false;
+        this.age = value;
+        if (value >= 25) {
+            this.requestNewPhoto();
+        }
+        return value;
+    }
+
+    static createEmpty() {
+        return new User('');
+    }
+}
+
+const user1 = new User('Leonid Pistryi');
+
+console.log(user1);
