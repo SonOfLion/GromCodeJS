0ce792e290930180e830a626ea65e3c2980849a0
SonOfLion <syplusplus@gmail.com>
2020-05-28 17:43:39 +0300

    2

---
+++ Lesson20/Task2/index.js
@@ -0,0 +1,36 @@
+export class Vehicle {
+    constructor(name, numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+        this.name = name;
+    }
+
+    move() {
+        console.log(`${this.name} is moving`);
+    }
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+};
+
+export class Ship extends Vehicle {
+    constructor(name, numberOfWheels, maxSpeed) {
+        super(name, numberOfWheels);
+        this.maxSpeed = maxSpeed;
+    }
+
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        super.move();
+    }
+    stopmachine() {
+        super.stop();
+        console.log(`${this.name} lifting anchor down`);
+    }
+};
+
+const ship1 = new Ship('Aurora', 18);
+
+ship1.startMachine();
+console.log(ship1);
+ship1.move();
+ship1.stop();
