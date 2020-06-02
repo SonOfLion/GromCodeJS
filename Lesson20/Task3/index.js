64b4ed04ae13f121289f4869ba260b06682bc005
SonOfLion <syplusplus@gmail.com>
2020-06-02 06:25:03 +0300

    3

---
+++ Lesson20/Task3/index.js
@@ -0,0 +1,32 @@
+//wallet
+
+export class Wallet {
+    constructor() {
+        this._balance = 0; //_ -- mean private
+    }
+
+    getBalance() {
+        return this._balance;
+    }
+
+    deposit(amount) {
+        this._balance += amount;
+    }
+
+    withdraw(amount) {
+        if (amount > this._balance) {
+            console.log('No enough founds');
+            return;
+        }
+        this._balance -= amount;
+    }
+}
+
+// const wallet1 = new Wallet();
+
+// console.log(wallet1.getBalance());
+// wallet1.deposit(45);
+// console.log(wallet1.getBalance());
+// wallet1.withdraw(34);
+// console.log(wallet1.getBalance());
+// wallet1.withdraw(12);
