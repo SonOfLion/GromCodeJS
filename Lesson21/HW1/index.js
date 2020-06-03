b136a8c629f74c71b4f890ac8b2d752fc625a64a
SonOfLion <syplusplus@gmail.com>
2020-06-03 15:11:11 +0300

    1

---
+++ Lesson21/HW1/index.js
@@ -0,0 +1,16 @@
+function getTitleElement() {
+    const titleElem = document.querySelector('.title');
+
+    console.dir(titleElem);
+    return titleElem;
+}
+
+function getInputElement() {
+    const getInput = document.querySelector('.text');
+
+    console.dir(getInput);
+    return getInput;
+}
+
+getInputElement();
+getTitleElement();
