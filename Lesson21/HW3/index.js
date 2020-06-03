400bd313dbc8145ddbd2d27bd502a9aa8c7006ef
SonOfLion <syplusplus@gmail.com>
2020-06-03 17:58:05 +0300

    3

--- Lesson21/HW3/index.js
+++ Lesson21/HW3/index.js
@@ -1,28 +1,24 @@
 function getTitle() {
     const textCont = document.querySelector('.title').textContent;
 
-    console.dir(textCont);
     return textCont;
 }
 
 function getDescription() {
     const desc = document.querySelector('.about').innerText;
 
-    console.dir(desc);
     return desc;
 }
 
 function getPlans() {
     const plan = document.querySelector('.plans').innerHTML;
 
-    console.dir(plan);
     return plan;
 }
 
 function getGoal() {
     const goal = document.querySelector('.goal').outerHTML;
 
-    console.dir(goal);
     return goal;
 }
 
