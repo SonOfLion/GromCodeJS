72cbe81b3a4dd7e0a5265dd95cd7cfbcfa805531
SonOfLion <syplusplus@gmail.com>
2020-06-04 07:49:37 +0300

    8

---
+++ Lesson21/HW8/index.js
@@ -0,0 +1,23 @@
+function finisList() {
+    const firstElem = document.createElement('li');
+    const fourthElem = document.createElement('li');
+    const sixElem = document.createElement('li');
+    const eigthElem = document.createElement('li');
+
+    firstElem.textContent = '1';
+    fourthElem.textContent = '4';
+    sixElem.textContent = '6';
+    eigthElem.textContent = '8';
+
+    const list = document.querySelector('.list');
+    list.append(eigthElem);
+    list.prepend(firstElem);
+
+    const spec = document.querySelector('.special');
+    spec.before(fourthElem);
+    spec.after(sixElem);
+}
+
+// finisList();
+
+export { finisList };
