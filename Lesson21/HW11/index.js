0a33232ba1f81ab0a2dd780da8f5583e840075d8
SonOfLion <syplusplus@gmail.com>
2020-06-04 17:53:03 +0300

    11

--- Lesson21/HW11/index.js
+++ Lesson21/HW11/index.js
@@ -1,7 +1,9 @@
-export function squaredNumbers() {
+export const squaredNumbers = () => {
     document.querySelectorAll('.number').
     forEach((elem) => {
         const mult = elem.dataset.number ** 2;
         elem.dataset.squaredNumbers = mult;
     });
-}
+};
+
+// squaredNumbers();
