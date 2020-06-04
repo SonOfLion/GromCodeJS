e8e8d8dff9d6abc2c3fbce571f09364755fd7a6c
SonOfLion <syplusplus@gmail.com>
2020-06-04 17:09:01 +0300

    11

--- Lesson21/HW11/index.js
+++ Lesson21/HW11/index.js
@@ -2,7 +2,7 @@ function squaredNumbers() {
     const num = document.querySelectorAll('.number');
 
     for (let elem of num) {
-        const mult = elem * elem;
+        const mult = elem.dataset.number * elem.dataset.number;
         elem.dataset.squaredNumbers = mult;
     }
 }
