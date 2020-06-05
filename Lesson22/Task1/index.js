9f89050a7587d7bf8b1f95ef3bbd2e7436f261a9
SonOfLion <syplusplus@gmail.com>
2020-06-05 18:51:03 +0300

    1

--- Lesson22/Task1/index.js
+++ Lesson22/Task1/index.js
@@ -1,52 +1,50 @@
-const divElem = document.querySelector('.rect_div');
-const pElem = document.querySelector('.rect_p');
-const spanElem = document.querySelector('.rect_span');
-const clearBtn = document.querySelector('.clear-btn');
-const removeBtn = document.querySelector('.remove-handlers-btn');
-const attatchBtn = document.querySelector('.attach-handlers-btn');
+const divElem = document.querySelector(".rect_div");
+const pElem = document.querySelector(".rect_p");
+const spanElem = document.querySelector(".rect_span");
+const eventsListElem = document.querySelector(".events-list");
 
 const logTarget = (text, color) => {
-    const eventList = document.querySelector('.events-list');
-    eventList.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
+    eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
 };
 
-const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
-const logGreenP = logTarget.bind(null, 'P', 'green');
-const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+const logGreenDiv = logTarget.bind(null, "DIV", "green");
+const logGreenP = logTarget.bind(null, "P", "green");
+const logGreenSpan = logTarget.bind(null, "SPAN", "green");
 
-const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
-const logGreyP = logTarget.bind(null, 'P', 'grey');
-const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
+const logGreyDiv = logTarget.bind(null, "DIV", "grey");
+const logGreyP = logTarget.bind(null, "P", "grey");
+const logGreySpan = logTarget.bind(null, "SPAN", "grey");
 
-const attatchEventList = () => {
-    divElem.addEventListener('click', logGreyDiv, true);
-    pElem.addEventListener('click', logGreyP, true);
-    spanElem.addEventListener('click', logGreySpan, true);
+const btnAttachHandleds = document.querySelector(".attach-handlers-btn");
+const attachHandleds = () => {
+    divElem.addEventListener("click", logGreyDiv, { capture: true });
+    divElem.addEventListener("click", logGreenDiv);
 
-    spanElem.addEventListener('click', logGreenSpan);
-    pElem.addEventListener('click', logGreenP);
-    divElem.addEventListener('click', logGreenDiv);
+    pElem.addEventListener("click", logGreyP, { capture: true });
+    pElem.addEventListener("click", logGreenP);
+
+    spanElem.addEventListener("click", logGreySpan, { capture: true });
+    spanElem.addEventListener("click", logGreenSpan);
 };
 
-attatchEventList();
+attachHandleds();
+btnAttachHandleds.addEventListener("click", attachHandleds);
 
-const pushAttatch = attatchEventList;
-attatchBtn.addEventListener('click', pushAttatch);
-
-const removeEventList = () => {
-    divElem.removeEventListener('click', logGreyDiv, true);
-    pElem.removeEventListener('click', logGreyP, true);
-    spanElem.removeEventListener('click', logGreySpan, true);
-
-    spanElem.removeEventListener('click', logGreenSpan);
-    pElem.removeEventListener('click', logGreenP);
-    divElem.removeEventListener('click', logGreenDiv);
+const btnClear = document.querySelector(".clear-btn");
+const clearEventsList = () => {
+    eventsListElem.innerHTML = null;
 };
-const pushRemove = removeEventList;
-removeBtn.addEventListener('click', pushRemove);
+btnClear.addEventListener("click", clearEventsList);
 
-const clearEventList = () => {
-    document.querySelector('.events-list').innerHTML = '';
+const btnRemoveHandleds = document.querySelector(".remove-handlers-btn");
+const disableHandleds = () => {
+    divElem.removeEventListener("click", logGreyDiv, true);
+    divElem.removeEventListener("click", logGreenDiv);
+
+    pElem.removeEventListener("click", logGreyP, { capture: true });
+    pElem.removeEventListener("click", logGreenP);
+
+    spanElem.removeEventListener("click", logGreySpan, { capture: true });
+    spanElem.removeEventListener("click", logGreenSpan);
 };
-const pushClear = clearEventList;
-clearBtn.addEventListener('click', pushClear);
+btnRemoveHandleds.addEventListener("click", disableHandleds);
