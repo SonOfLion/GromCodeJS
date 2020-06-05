953c49ecfbeab7b47cb4a48edcb53af42973e81d
SonOfLion <syplusplus@gmail.com>
2020-06-06 00:10:03 +0300

    1

---
+++ Lesson22/HW1/index.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('.create-btn');
+
+button.addEventListener('click', () => {
+    alert('done');
+});
+
+// const buttonElem = document.querySelector('.create-btn');
+// buttonElem.onClick = function() {
+//     alert('clicked');
+// }
