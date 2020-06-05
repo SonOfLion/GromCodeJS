1fc2791103ede9f09dc9e2b86cf39db1a10f0586
SonOfLion <syplusplus@gmail.com>
2020-06-05 18:30:26 +0300

    1

--- Lesson21/HW12/index.js
+++ Lesson21/HW12/index.js
@@ -1,9 +1,2 @@
-export function getSection(num) {
-    const span = document.querySelector(`span[data-number='${num}']`);
-    const parent = span.closest('.box');
-
-    console.dir(parent.dataset.section);
-    return parent.dataset.section;
-}
-
-// getSection(3);
+function test() {};
+const test1 = () => {};
