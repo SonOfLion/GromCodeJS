a0303fa1bde89985d68f073fecaad0c479cc5b48
SonOfLion <syplusplus@gmail.com>
2020-06-09 21:53:30 +0300

    1

--- Lesson23/HW1/index.js
+++ Lesson23/HW1/index.js
@@ -4,10 +4,10 @@
 
 export function getDiff(startDate, endDate) {
     const dif = Math.abs(startDate - endDate);
-    const difDays = Math.round(dif / (1000 * 60 * 60 * 24));
-    const difHours = Math.round(dif / (1000 * 60 * 60) % 24);
-    const difMinutes = Math.round(dif / (1000 * 60) % 60);
-    const difSeconds = Math.round(dif / 1000) % 60;
+    const difDays = Math.floor(dif / (1000 * 60 * 60 * 24));
+    const difHours = Math.floor(dif / (1000 * 60 * 60) % 24);
+    const difMinutes = Math.floor(dif / (1000 * 60) % 60);
+    const difSeconds = Math.floor(dif / 1000) % 60;
     return `${difDays}d ${difHours}h ${difMinutes}m ${difSeconds}s`;
 }
 
