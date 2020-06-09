36534c6aebb80ea0d650f1105c46b5e6e0b64c30
SonOfLion <syplusplus@gmail.com>
2020-06-09 06:06:51 +0300

    1

--- Lesson24/Task1/index.js
+++ Lesson24/Task1/index.js
@@ -3,7 +3,6 @@ const weekDays = ['Su', 'Mon', 'Tu', 'We', 'Th', 'Fr', 'St'];
 export const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
-
     return weekDays[new Date(dateInFuture).getDay()];
 };
 
