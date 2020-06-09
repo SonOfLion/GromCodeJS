5bc0ac5673429ebc1c14a0d15dbb8709745fcc7b
SonOfLion <syplusplus@gmail.com>
2020-06-10 02:48:01 +0300

    2

--- Lesson23/HW3/index.js
+++ Lesson23/HW3/index.js
@@ -49,21 +49,29 @@ const tasks = [{
 ];
 
 const listElem = document.querySelector('.list');
+
+const doneTask = (a, b) => {
+    if (a.done - b.done !== 0) {
+        return a.done - b.done;
+    }
+    console.log(doneTask)
+    if (a.done === true) {
+        return new Date(b.doneDate) - new Date(a.doneDate);
+    }
+    return new Date(b.date) - new Date(a.date);
+};
+
 const renderTasks = (tasksList) => {
     listElem.innerHTML = '';
     const tasksElems = tasksList
         .slice()
-        .sort((a, b) => {
-            if (a.done !== b.done) return a.done - b.done
-            if (a.done) return new Date(b.doneDate) - new Date(a.doneDate)
-            return new Date(b.date) - new Date(a.date)
-        })
+        .sort(doneTask)
         .map((task) => {
             const listItemElem = document.createElement('li');
             listItemElem.classList.add('list__item');
             const checkbox = document.createElement('input');
             checkbox.setAttribute('type', 'checkbox');
-            checkbox.setAttribute('data-task-id', task.id);
+            checkbox.setAttribute('data-id', task.id);
             checkbox.checked = task.done;
             checkbox.classList.add('list__item-checkbox');
             if (task.done) {
@@ -72,41 +80,46 @@ const renderTasks = (tasksList) => {
             listItemElem.append(checkbox, task.text);
             return listItemElem;
         });
-    //console.log(tasksElems)
     listElem.append(...tasksElems);
 };
+
 renderTasks(tasks);
-
-const inputAdd = document.querySelector('.task-input')
-
-const createBtn = document.querySelector('.btn');
-createBtn.addEventListener('click', createTask);
-
-function createTask(event) {
-    const input = document.querySelector('.task-input');
-    if (!input.value) return;
-    tasks.push({
-        text: input.value,
-        done: false,
-        id: `${tasks.length + 1}`,
-        date: new Date(),
-        doneDate: null
-    })
-
-    input.value = '';
-    renderTasks(tasks);
-}
 
 listElem.addEventListener('click', updateTask);
 
 function updateTask(event) {
     const classes = event.target.classList;
-    const checkbox = event.target;
-    if (!classes.contains('list__item-checkbox')) return;
 
-    const task = tasks.find(task => task.id === event.target.dataset.taskId);
-    task.done = checkbox.checked;
-    task.doneDate = new Date()
+    if (!classes.contains('list__item-checkbox')) {
+        return;
+    }
+
+    const task = tasks.find(task => task.id === event.target.dataset.id);
+
+    task.done = event.target.checked;
+    task.doneDate = new Date();
+
+    renderTasks(tasks);
+}
+
+const createBtn = document.querySelector('.btn');
+createBtn.addEventListener('click', createTask);
+
+function createTask() {
+    const input = document.querySelector('.task-input');
+    const inputValue = input.value;
+
+    if (!inputValue) return;
+
+    tasks.push({
+        text: inputValue,
+        done: false,
+        id: `${tasks.length + 1}`,
+        date: new Date(),
+        doneDate: null
+    });
+
+    input.value = '';
 
     renderTasks(tasks);
 }
