dcab757b1c2fd2f60897ff431e9bacccbb766993
SonOfLion <syplusplus@gmail.com>
2020-06-12 15:37:29 +0300

    1

---
+++ Lesson28/Task1/index.js
@@ -0,0 +1,30 @@
+function calc(initValue) {
+    let result = initValue;
+
+    const calculator = {
+        add(value) {
+            result += value;
+            return calculator;
+        },
+        substract(value) {
+            result -= value;
+            return this;
+        },
+        mult(value) {
+            result *= value;
+            return this;
+        },
+        div(value) {
+            result /= value;
+            return this;
+        },
+        result() {
+            return result;
+        },
+    };
+    return calculator;
+}
+
+const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+
+console.log(result);
