a871fd01f76c41508cd6c5d40ac510f5b18b8da0
SonOfLion <syplusplus@gmail.com>
2020-06-13 16:10:27 +0300

    3

---
+++ Lesson28/Task3/index.js
@@ -0,0 +1,10 @@
+// const sum = (a, b) => a + b;
+
+export const mult = a => b => a * b;
+
+export const twice = mult(2);
+
+export const triple = mult(3);
+
+// console.log(twice(2));
+// console.log(triple(3));
