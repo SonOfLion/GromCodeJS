5ed8dab33bc2ae073c00f973496ff7e83c4b3fe9
SonOfLion <syplusplus@gmail.com>
2020-06-13 12:31:02 +0300

    2

--- Lesson28/Task2/index.js
+++ Lesson28/Task2/index.js
@@ -1,24 +1,24 @@
-const favorites = ['id-6', 'id-17'];
+// const favorites = ['id-6', 'id-17'];
 
-const tree = {
-    id: 'id-1',
-    name: 'Products',
-    nodes: [{
-            id: 'id-2',
-            name: 'Food',
-            nodes: [{
-                id: 'id-6',
-                name: 'Drinks',
-                nodes: []
-            }]
-        },
-        {
-            id: 'id-17',
-            name: 'Vehicles',
-            nodes: []
-        }
-    ],
-};
+// const tree = {
+//     id: 'id-1',
+//     name: 'Products',
+//     nodes: [{
+//             id: 'id-2',
+//             name: 'Food',
+//             nodes: [{
+//                 id: 'id-6',
+//                 name: 'Drinks',
+//                 nodes: []
+//             }]
+//         },
+//         {
+//             id: 'id-17',
+//             name: 'Vehicles',
+//             nodes: []
+//         }
+//     ],
+// };
 
 export const markFavorites = (tree, favorites) => {
     const isFavorite = favorites.includes(tree.id);
