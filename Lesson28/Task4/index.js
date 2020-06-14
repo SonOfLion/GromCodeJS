d811cec9859647edb7eebd25c392e35379734686
SonOfLion <syplusplus@gmail.com>
2020-06-14 13:58:54 +0300

    4

---
+++ Lesson28/Task4/index.js
@@ -0,0 +1,27 @@
+//compose
+
+//f
+
+//g
+
+//f(g(value));
+
+//f * g(value);
+
+// compose(func1,func2,...)=>
+
+const add3 = value => value + 3;
+const mult2 = value => value * 2;
+const div4 = value => value / 4;
+
+export const compose = (...funcs) => value => {
+    return funcs.reduce((acc, func) => func())
+};
+
+const doEveryThing = compose(
+    add3,
+    mult2,
+    div4,
+);
+
+console.log(doEveryThing(3));
