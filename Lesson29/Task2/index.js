200adb1defc03839436787769159d61db3cadc4b
SonOfLion <syplusplus@gmail.com>
2020-06-15 11:59:56 +0300

    2

---
+++ Lesson29/Task2/index.js
@@ -0,0 +1,23 @@
+//pinger(num,period)
+
+const pinger = (num, period) => {
+    console.log('Ping');
+
+    let i = num;
+
+    const interval = setInterval(() => {
+        if (--i > 0) {
+            console.log('Ping');
+        } else {
+            clearInterval(interval)
+        }
+    }, period);
+
+    // setTimeout(() => {
+    //     clearInterval(interval);
+    // }, num * per);
+
+    console.log(interval);
+};
+
+pinger(5, 1000);
