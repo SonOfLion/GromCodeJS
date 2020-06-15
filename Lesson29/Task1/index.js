46d24990a82806d6322e0bce2e69f195acd6d67a
SonOfLion <syplusplus@gmail.com>
2020-06-15 11:05:21 +0300

    1

---
+++ Lesson29/Task1/index.js
@@ -0,0 +1,29 @@
+//addImage
+
+function addImage(imgSrc, callback) {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My photo');
+
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    function onImageLoaded() {
+        const { width, height } = imgElem;
+        callback(null, { width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => callback('Image load failed'));
+}
+
+function onImageLoaded(error, data) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    const { width, height } = data;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`;
+}
