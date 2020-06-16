25b758b7a339aa662cb1fe8fd9f8ca500145e165
SonOfLion <syplusplus@gmail.com>
2020-06-16 20:30:19 +0300

    3

---
+++ Lesson30/HW2/index.js
@@ -0,0 +1,29 @@
+/* ===> 1 <=== */
+
+/*
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise
+ */
+
+const successPromise = new Promise(resolve => {
+    resolve(67);
+});
+// console.log(successPromise);
+// console.log(typeof successPromise);Object
+/*
+ * допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+ * чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
+ */
+
+successPromise.then(function onSuccess(number) {
+    console.log(number * number);
+});
+
+/*
+ * в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+ * текст вывелся в консоль?
+ * подумайте почему
+ */
+successPromise.catch(function onError() {
+    console.log('I am an error', onError);
+});
