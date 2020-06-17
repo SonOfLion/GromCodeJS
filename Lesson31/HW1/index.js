4f14ed4ee6686f9b80874a067965e93cf004906b
SonOfLion <syplusplus@gmail.com>
2020-06-17 21:29:21 +0300

    3

---
+++ Lesson31/HW1/index.js
@@ -0,0 +1,36 @@
+const successPromise = new Promise(resolve => {
+    resolve(32);
+});
+
+/*
+ * исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+    .then(number => {
+        const halfNumber = number / 2;
+        return halfNumber;
+    })
+    .then(number => {
+        /* fix this handler */
+        const squaredNumber = number * number;
+        return number;
+    })
+    .then(result => {
+        console.log(result); // 256
+    });
+
+/*
+ * исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+    .then(number => {
+        /* fix this handler */
+        return number * 10;
+        console.log(number);
+    })
+    .then(result => {
+        console.log(result); // 320
+    });
+
+console.log('!!! Обратите внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!');
