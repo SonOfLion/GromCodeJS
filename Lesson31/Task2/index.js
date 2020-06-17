9228e3d7d95c40846f61b9bfabdd97ee20980618
SonOfLion <syplusplus@gmail.com>
2020-06-17 18:41:55 +0300

    2

---
+++ Lesson31/Task2/index.js
@@ -0,0 +1,24 @@
+//asyncCalculator
+
+export const asyncCalculator = numbr => new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Initial value: ${numbr}`)
+            resolve(numbr);
+        }, 500)
+    })
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+        }, 500)
+    }))
+    .then(value =>
+        new Promise((resolve) => {
+            const result = value * 2;
+            console.log(`Doubled value: ${result}`);
+            return value;
+        }));
+
+
+// asyncCalculator(4);
