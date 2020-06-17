39f98477cd15d99a5c304e09759511b9f30a61a5
SonOfLion <syplusplus@gmail.com>
2020-06-17 14:04:13 +0300

    1

---
+++ Lesson31/Task1/index.js
@@ -0,0 +1,22 @@
+export const requestUserData = (userId) => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    email: `${userId}`,
+                    userId
+                });
+            }, 1000);
+        }
+    });
+    return request;
+};
+
+requestUserData('user-1')
+    .then(data => console.log(data));
