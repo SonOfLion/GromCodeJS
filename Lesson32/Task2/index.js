6d7c99d442c36d3200261e4e9a8ed4ce1e64fbd2
SonOfLion <syplusplus@gmail.com>
2020-06-19 17:33:46 +0300

    2

--- Lesson32/Task2/index.js
+++ Lesson32/Task2/index.js
@@ -1,3 +1,9 @@
+const servers = [
+    'httpf://server.com/us',
+    'httpf://server.com/eu',
+    'httpf://server.com/au',
+];
+
 const request = url => new Promise(resolve => {
     setTimeout(() => {
         resolve({
@@ -7,14 +13,8 @@ const request = url => new Promise(resolve => {
             },
             source: url
         });
-    }, Math.random() * 3000 + 1000);
+    }, Math.random() * 1000 + 3000);
 });
-
-const servers = [
-    'httpf://server.com/us',
-    'httpf://server.com/eu',
-    'httpf://server.com/au',
-];
 
 export const getUserASAP = (userId) => {
     const userUrls = servers
@@ -30,3 +30,5 @@ export const getUserASAP = (userId) => {
 
 // getUserASAP('user-id-1')
 //     .then(res => console.log(res));
+
+export { getUserASAP };
