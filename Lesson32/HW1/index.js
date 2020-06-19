22ed439fcad069b197a1a115e0b66378bad63186
SonOfLion <syplusplus@gmail.com>
2020-06-19 18:00:43 +0300

    2

--- Lesson32/HW1/index.js
+++ Lesson32/HW1/index.js
@@ -8,12 +8,12 @@ export const makePromise = new Promise(resolve => {
     resolve(17);
 });
 
-const makePromise = Promise.resolve(17);
+// const makePromise = Promise.resolve(17);
 
 /*
  * пример использования
  */
-makePromise(17)
-    .then(number => {
-        console.log(number); // 17
-    });
+// makePromise(17)
+//     .then(number => {
+//         console.log(number); // 17
+//     });
