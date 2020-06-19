276460848f4e4f1133d27221c3ca44bb2819c6ae
SonOfLion <syplusplus@gmail.com>
2020-06-19 17:27:40 +0300

    2

--- Lesson32/Task2/index.js
+++ Lesson32/Task2/index.js
@@ -7,7 +7,7 @@ const request = url => new Promise(resolve => {
             },
             source: url
         });
-    }, Math.random() * 1000 + 3000);
+    }, Math.random() * 3000 + 1000);
 });
 
 const servers = [
@@ -19,12 +19,14 @@ const servers = [
 export const getUserASAP = (userId) => {
     const userUrls = servers
         .map(serverUrl => `${serverUrl}/users/${userId}`);
+    // console.log(userUrls);
 
     const requests = userUrls
         .map(userUrl => request(userUrl));
+    // console.log(requests);
 
     return Promise.race(requests);
 };
 
-// getUserASAP('user-id-1')
-//     .then(res => console.log(res));
+getUserASAP('user-id-1')
+    .then(res => console.log(res));
