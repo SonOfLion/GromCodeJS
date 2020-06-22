c2d44ce5026595f92a0abd5ed125f057ef3fd616
SonOfLion <syplusplus@gmail.com>
2020-06-22 16:24:08 +0300

    2

--- Lesson33/HW1/index.js
+++ Lesson33/HW1/index.js
@@ -2,14 +2,14 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
 export const getTasksList = () => fetch(baseUrl)
-    .then(tasksList => tasksList.json());
+    .then(response => response.json());
 
 // getTasksList(baseUrl)
 //     .then(tasksList => console.log(tasksList));
 
 /* getTaskById code here */
 const getTaskById = (taskId) => fetch(`${baseUrl}/${taskId}`)
-    .then(taskId => taskId.json());
+    .then(response => response.json());
 
 // getTaskById(2)
 //     .then(taskData => console.log(taskData));
