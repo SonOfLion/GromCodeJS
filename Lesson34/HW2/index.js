dd5e1c18e0d032da429d6f94e9b4d530d0fe6758
SonOfLion <syplusplus@gmail.com>
2020-06-25 23:54:12 +0300

    3

---
+++ Lesson34/HW2/index.js
@@ -0,0 +1,33 @@
+const baseUrl = 'https://5ef46efeac6d1e00168ca095.mockapi.io/api/v1/users';
+
+const formInput = document.querySelectorAll('.form-input');
+const errorText = document.querySelector('.error-text');
+const submitButton = document.querySelector('.submit-button');
+const formLogin = document.querySelector('.login-form');
+errorText.textContent = '';
+
+const validData = () => {
+    formLogin.reportValidity() ? submitButton.disable = false : submitButton.disable = true;
+};
+
+const sendDataForm = event => {
+    event.preventDefault();
+    const formData = [...new FormData(formLogin)]
+        .reduce((acc, [field, value]) => ({...acc, [field]: value }), {});
+
+    fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+
+    .then(response => response.json())
+        .then(userData => {
+            inputs.map(elem => elem.value = '');
+            submitButton.disabled = true;
+            alert(JSON.stringify(userData));
+        })
+        .catch(() => errorText.textContent = 'Failed to create user')
+}
