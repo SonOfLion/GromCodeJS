015e4a5d9a8a425e3366efb6c01c5383161aa30b
SonOfLion <syplusplus@gmail.com>
2020-06-26 20:23:30 +0300

    3

--- Lesson34/HW2/index.js
+++ Lesson34/HW2/index.js
@@ -1,36 +1,43 @@
 const baseUrl = 'https://5ef46efeac6d1e00168ca095.mockapi.io/api/v1/users';
 
-const formInput = [...document.querySelectorAll('.form-input')];
-const errorText = document.querySelector('.error-text');
-const submitButton = document.querySelector('.submit-button');
-const formLogin = document.querySelector('.login-form');
+const formInputElem = document.querySelectorAll('.form-input');
+// console.log(formInputElem);
+const errorTextElem = document.querySelector('.error-text');
+const submitButtonElem = document.querySelector('.submit-button');
+const formLoginElem = document.querySelector('.login-form');
 
+const validate = () => {
+    formLoginElem.reportValidity() ?
+        submitButtonElem.disabled = false :
+        submitButtonElem.disabled = true;
 
-const validData = () => {
-    formLogin.reportValidity() ? submitButton.disable = false : submitButton.disable = true;
-    errorText.textContent = '';
+    errorTextElem.textContent = '';
 };
 
-const sendDataForm = (event) => {
+formLoginElem.addEventListener('input', validate)
+
+const postUserData = (event) => {
     event.preventDefault();
 
-    const dataOfUsers = [...new FormData(formLogin)]
-        .reduce((acc, [field, value]) => ({...acc, [field]: value }), {});
+    const formData = [...new FormData(formLoginElem)]
+        .reduce((acc, [input, value]) => ({...acc, [input]: value }), {});
 
     fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(dataOfUsers),
-    })
-
-    .then(response => response.json())
-        .then(userData => {
-            formInput.map(elem => elem.value = '');
-            submitButton.disabled = true;
-            alert(JSON.stringify(userData));
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
         })
-
-    .catch(() => errorText.textContent = 'Failed to create user')
+        .then(response => response.json())
+        .then(response => {
+            formInputElem.forEach(el => el.value = '')
+            submitButtonElem.disabled = true;
+            alert(JSON.stringify(response));
+        })
+        .catch(() => errorTextElem.textContent = 'Failed to create user');
 };
+
+formLoginElem.addEventListener('submit', postUserData);
+// console.log(formLoginElem);
+// console.log(formData);
